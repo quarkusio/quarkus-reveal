5a13c33d3ca77b7b5299e6247b14e749a578503d
Andy Damevin <ia3andy@gmail.com>
2024-05-16 15:46:20 +0200

    Use picocli

--- QuarkusReveal.java
+++ QuarkusReveal.java
@@ -1,4 +1,5 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS info.picocli:picocli:4.6.3
 //DEPS io.quarkus.platform:quarkus-bom:3.10.0@pom
 //DEPS io.quarkus:quarkus-rest
 //DEPS io.quarkiverse.web-bundler:quarkus-web-bundler:1.5.0.CR1
@@ -13,71 +14,87 @@
 //Q:CONFIG quarkus.http.port=7979
 
 import io.quarkus.runtime.Quarkus;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import io.vertx.core.http.impl.MimeMapping;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import picocli.CommandLine;
+
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.concurrent.Callable;
 
 
-@Path("/")
-@ApplicationScoped
-public class QuarkusReveal {
 
-    public static void main(String[] args) {
-        Quarkus.run(args);
+@CommandLine.Command(name = "quarkus-reveal", mixinStandardHelpOptions = true, version = "0.1",
+        description = "Develop and use your Reveal.js decks easily with Quarkus")
+public class QuarkusReveal implements Callable<Integer> {
+
+    @CommandLine.Parameters(index = "0", description = "The greeting to print", defaultValue = "./demo.md")
+    private String deck;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new QuarkusReveal()).execute(args);
+        System.exit(exitCode);
     }
 
-    @ConfigProperty(name = "deck", defaultValue = "./demo.md")
-    String deck;
-
-    @GET
-    @Path("demo.md")
-    @Produces("text/markdown")
-    public String getDeck() throws IOException {
-        final java.nio.file.Path demo = java.nio.file.Path.of(deck);
-        if(!Files.exists(demo)) {
-            throw new IOException("Deck file not found: " + demo);
-        }
-        return Files.readString(demo);
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        System.setProperty("deck", deck);
+        Quarkus.run();
+        return 0;
     }
 
-    @GET
-    @Path("assets/{name}")
-    public Response getAsset(@PathParam("name") String name) throws IOException {
-        final java.nio.file.Path deckPath = java.nio.file.Path.of(deck).toAbsolutePath();
-        final java.nio.file.Path assets = deckPath.getParent().resolve("assets");
-        byte[] asset = findAsset(name, assets);
-        if (asset == null)
-            asset = findAsset(name, deckPath.getParent().getParent().resolve("assets"));
-        if (asset == null) {
-            throw new IOException("Deck asset not found: " + name);
+    @ApplicationScoped
+    @Path("/")
+    public static class RestResource {
+
+        @ConfigProperty(name = "deck")
+        String deck;
+
+        @GET
+        @Path("demo.md")
+        @Produces("text/markdown")
+        public String getDeck() throws IOException {
+            final java.nio.file.Path demo = java.nio.file.Path.of(deck);
+            if(!Files.exists(demo)) {
+                throw new IOException("Deck file not found: " + demo);
+            }
+            return Files.readString(demo);
         }
-        return Response.ok()
-                .entity(asset)
-                .type(MimeMapping.getMimeTypeForFilename(name))
-                .build();
+
+        @GET
+        @Path("assets/{name}")
+        public Response getAsset(@PathParam("name") String name) throws IOException {
+            final java.nio.file.Path deckPath = java.nio.file.Path.of(deck).toAbsolutePath();
+            final java.nio.file.Path assets = deckPath.getParent().resolve("assets");
+            byte[] asset = findAsset(name, assets);
+            if (asset == null)
+                asset = findAsset(name, deckPath.getParent().getParent().resolve("assets"));
+            if (asset == null) {
+                throw new IOException("Deck asset not found: " + name);
+            }
+            return Response.ok()
+                    .entity(asset)
+                    .type(MimeMapping.getMimeTypeForFilename(name))
+                    .build();
+        }
+
+        private static byte[] findAsset(String name, java.nio.file.Path assets) throws IOException {
+            if(!Files.isDirectory(assets)) {
+                return null;
+            }
+            final java.nio.file.Path asset = assets.resolve(name);
+            if(!Files.isRegularFile(asset)) {
+                return null;
+            }
+            return Files.readAllBytes(asset);
+        }
     }
 
-    private static byte[] findAsset(String name, java.nio.file.Path assets) throws IOException {
-        if(!Files.isDirectory(assets)) {
-            return null;
-        }
-        final java.nio.file.Path asset = assets.resolve(name);
-        if(!Files.isRegularFile(asset)) {
-            return null;
-        }
-        return Files.readAllBytes(asset);
-    }
+
 }
